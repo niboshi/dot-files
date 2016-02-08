0a2a93527926df5cf67b578e335d1f139cfa505e
niboshi <niboshi000@gmail.com>
2016-02-08 17:04:12 +0900

    .emacs.d: misc...
    - put-other-buffer-behind
    - projectile
    - Show message buffer at start
    - fill-column-indicator

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,6 +5,9 @@
 ;-----------------------
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
+(setq niboshi-hotkey-prefix (kbd "C-c ;"))
+(defun niboshi-make-hotkey (k) (concat niboshi-hotkey-prefix (kbd k)))
+
 ;-----------------------
 ; Disable original features
 ;-----------------------
@@ -17,7 +20,7 @@
 (setq w32-pass-alt-to-system 1)
 
 ; Disable IME hotkey
-(global-set-key (kbd "C-\\") 'ignore)
+(global-unset-key (kbd "C-\\"))
 
 ; Disable mouse commands
 (global-set-key [down-mouse-2] 'ignore)
@@ -46,8 +49,8 @@
 (global-set-key [M-down-mouse-3] 'ignore)
 (global-set-key [M-mouse-3] 'ignore)
 
-(global-set-key [mouse-4] '(lambda () (interactive) (scroll-down-command 1)))
-(global-set-key [mouse-5] '(lambda () (interactive) (scroll-up-command 1)))
+(global-set-key [mouse-4] (lambda () (interactive) (scroll-down-command 1)))
+(global-set-key [mouse-5] (lambda () (interactive) (scroll-up-command 1)))
 
 ;-----------------------
 ; Essential keybinding
@@ -66,6 +69,7 @@
 
 ; C-h
 (define-key key-translation-map (kbd "C-h") "\C-?")
+(setq backward-delete-char-untabify-method nil) ; This prevents C-h from converting a tab to spaces.
 
 ; bs-show
 (if (fboundp 'bs-show)
@@ -75,9 +79,15 @@
 (global-set-key (kbd "M-h") 'backward-kill-word)
 
 ; Scroll without moving cursor
-(global-set-key (kbd "M-p") '(lambda () (interactive) (scroll-down-command 1)))
-(global-set-key (kbd "M-n") '(lambda () (interactive) (scroll-up-command 1)))
+(global-set-key (kbd "M-p") (lambda () (interactive) (scroll-down-command 1)))
+(global-set-key (kbd "M-n") (lambda () (interactive) (scroll-up-command 1)))
 
+;-----------------------
+; Essential
+;-----------------------
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(setq create-lockfiles nil)
 
 ;-----------------------
 ; UI
@@ -104,44 +114,13 @@
     (add-to-list 'default-frame-alist '(alpha 97 95))
 ))
 
-;
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-(setq create-lockfiles nil)
-(setq default-truncate-lines t)
-(global-linum-mode 1)
-(if (display-graphic-p)
-    (progn
-      (set-face-attribute 'linum nil :background nil :foreground "#444444")
-      )
-  (progn
-    (setq linum-format "%d ")
-    (set-face-attribute 'linum nil :background nil :foreground "brightblack")
-    ))
-(setq-default indent-tabs-mode nil)
+;-----------------------
+; Show message buffer to the right
+;-----------------------
+(set-window-buffer
+ (split-window nil nil "right")
+ "*Messages*")
 
-(add-hook 'c-mode-common-hook
-          '(lambda()
-             (setq tab-width 4)
-             (setq c-basic-offset tab-width)
-             (c-set-offset 'arglist-intro '++)
-             (setq indent-tabs-mode t)
-             (which-function-mode t) ; show function name
-             (if (fboundp 'whitespace-mode) (whitespace-mode t))))
-
-(add-hook 'python-mode-hook
-          '(lambda ()
-             (setq indent-tabs-mode t)
-             (setq tab-width 4)
-             (setq python-indent 4)
-             (which-function-mode t) ; show function name
-             (if (fboundp 'whitespace-mode) (whitespace-mode t))))
-
-(global-set-key (kbd "C-c p")
- (lambda () (interactive)
-   (progn
-     (kill-new buffer-file-name)
-     (message buffer-file-name))))
 
 ;-----------------------
 ; Scroll
@@ -169,6 +148,9 @@
     (package-install 'ggtags)
     (package-install 'replace+)
     (package-install 'neotree)
+    (package-install 'helm)
+    (package-install 'projectile)
+    (package-install 'helm-projectile)
     ))
 
 ;-----------------------
@@ -179,34 +161,80 @@
     (if (file-accessible-directory-p base-dir)
         (dolist (file (directory-files base-dir))
           (if (not (member file ignore-list))
-              (progn
-                (load-file (concat base-dir "/" file)))
-            )))))
+              (load-file (concat base-dir "/" file)))))))
 
 ;-----------------------
-; Evaluation hotkeys
+; Misc
 ;-----------------------
+(setq default-truncate-lines t)
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+
+; Show path hotkey
+(global-set-key (niboshi-make-hotkey "p")
+                (lambda () (interactive)
+                  (kill-new buffer-file-name)
+                  (message buffer-file-name)))
+
+; Evaluation hotkeys
 (global-set-key (kbd "C-c e e") 'eval-expression)
 (global-set-key (kbd "C-c e r") 'eval-region)
 (global-set-key (kbd "C-c e b") 'eval-buffer)
 
-;-----------------------
 ; Encoding
-;-----------------------
 (prefer-coding-system 'utf-8)
 
-;-----------------------
-; Editing
-;-----------------------
 ; Enable CamelCase-aware word editing.
 (global-subword-mode 1)
 
 ;-----------------------
+; Line number
+;-----------------------
+(global-linum-mode 1)
+(if (display-graphic-p)
+    (progn
+      (set-face-attribute 'linum nil :background nil :foreground "#444444")
+      )
+  (progn
+    (setq linum-format "%d ")
+    (set-face-attribute 'linum nil :background nil :foreground "brightblack")
+    ))
+
+;-----------------------
+; fill-column-indicator
+;-----------------------
+(if (display-graphic-p)
+    (when (require 'fill-column-indicator nil 'noerror)
+      (setq-default fci-always-use-textual-rule t) ; Default behaviour (draw line by image) causes line height problem.
+      (setq-default fci-rule-column 80)
+      (setq-default fci-rule-color "gray11")
+      (add-hook 'c-mode-common-hook 'fci-mode)))
+
+;-----------------------
+; Hooks
+;-----------------------
+(add-hook 'c-mode-common-hook
+          (lambda()
+            (setq tab-width 4)
+            (setq c-basic-offset tab-width)
+            (c-set-offset 'arglist-intro '++)
+            (setq indent-tabs-mode t)
+            (which-function-mode t) ; show function name
+            (if (fboundp 'whitespace-mode) (whitespace-mode t))))
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode t)
+            (setq tab-width 4)
+            (setq python-indent 4)
+            (which-function-mode t) ; show function name
+            (if (fboundp 'whitespace-mode) (whitespace-mode t))))
+
+;-----------------------
 ; ido-mode (Interactive buffer switch, etc.)
 ;-----------------------
-(ido-mode 1)
-(ido-everywhere 1)
-(setq ido-enable-prefix t)
+(ido-mode nil)
+(setq ido-enable-prefix nil)
 (setq ido-enable-flex-matching t)
 (setq ido-case-fold t) ; Ignore case
 ;(setq ido-use-virtual-buffers t)
@@ -229,12 +257,11 @@
 ; Here's workaround.
 (if (eq system-type 'windows-nt)
     (if (boundp 'niboshi-grep-windows-null-device-workaround)
-        (progn
-          (defadvice grep-compute-defaults (around grep-compute-defaults-advice-null-device activate)
-            ;;; This is neccessary for 'grep' command
-            (setq null-device "/dev/null")
-            ;;; This is neccessary for 'lgrep', 'rgrep', 'find-grep', etc.
-            (let ((null-device "/dev/null")) ad-do-it)))))
+        (defadvice grep-compute-defaults (around grep-compute-defaults-advice-null-device activate)
+          ;;; This is neccessary for 'grep' command
+          (setq null-device "/dev/null")
+          ;;; This is neccessary for 'lgrep', 'rgrep', 'find-grep', etc.
+          (let ((null-device "/dev/null")) ad-do-it))))
 
 ;-----------------------
 ; Disable startup "For information..." message
@@ -260,7 +287,7 @@
 
       (setq whitespace-style '(empty face tabs newline tab-mark newline-mark))
       ;(setq whitespace-style '(spaces tabs newline space-mark tab-mark newline-mark))
-      (global-set-key "\C-c=w" 'whitespace-mode)
+      (global-set-key (kbd "C-c = w") 'whitespace-mode)
 
       (if (display-graphic-p)
           (progn
@@ -277,9 +304,8 @@
 
   (progn
     (message "Whitespace is unavailable.")
-    (global-set-key "\C-c=w" 'ignore)
+    (global-set-key (kbd "C-c = w") 'ignore)
     ))
-
 
 ;-----------------------
 ; visible-bell
@@ -295,14 +321,24 @@
 ; incremental search
 ;-----------------------
 (add-hook 'isearch-mode-hook
-          (function
-           (lambda()
-             (define-key isearch-mode-map "\C-h" 'isearch-mode-help)
-             (define-key isearch-mode-map "\C-t" 'isearch-toggle-regexp)
-             (define-key isearch-mode-map "\C-c" 'isearch-toggle-case-fold)
-             (define-key isearch-mode-map "\C-j" 'isearch-edit-string))))
+          (lambda()
+            (define-key isearch-mode-map "\C-h" 'isearch-mode-help)
+            (define-key isearch-mode-map "\C-t" 'isearch-toggle-regexp)
+            (define-key isearch-mode-map "\C-c" 'isearch-toggle-case-fold)
+            (define-key isearch-mode-map "\C-j" 'isearch-edit-string)))
 
-(defcustom isearch-help-reminder "awaafw" "abc" :type 'string)
+;-----------------------
+; Put other buffer behind
+;-----------------------
+(defun niboshi-put-other-buffer-behind()
+  (interactive)
+  (let ((win (car (cdr (window-list)))))
+    (if win
+        (let ((buf (window-buffer win)))
+          (if buf
+              (replace-buffer-in-windows buf))))))
+
+(global-set-key (niboshi-make-hotkey "-") 'niboshi-put-other-buffer-behind)
 
 ;-----------------------
 ; ggtags
@@ -319,10 +355,11 @@
       (global-set-key (kbd "C-c g f") 'ggtags-find-file)
       (global-set-key (kbd "C-c g u") 'ggtags-update-tags)
       ; Put ggtags buffer behind
-      (global-set-key (kbd "C-c g -") '(lambda() (interactive)
-                                         (progn
-                                           (let ((buf (get-buffer "*ggtags-global*")))
-                                             (if buf (replace-buffer-in-windows buf))))))
+      (global-set-key (kbd "C-c g -") (lambda() (interactive)
+                                        (progn
+                                          (let ((buf (get-buffer "*ggtags-global*")))
+                                            (if buf
+                                                (replace-buffer-in-windows buf))))))
       ))
 
 ;-----------------------
@@ -334,7 +371,6 @@
 (add-hook 'compilation-mode-hook
           (lambda () 
             (progn
-              (linum-mode 0)
               (setq compilation-scroll-output 'first-error)
               (setq complation-auto-jump-to-first-error t)
               )))
@@ -346,6 +382,27 @@
  '(diff-added ((t (:foreground "green"))) 'now)
  '(diff-removed ((t (:foreground "red"))) 'now)
  )
+
+;-----------------------
+; Projectile
+;-----------------------
+(when (require 'helm-projectile nil 'noerror)
+  (projectile-global-mode)
+  (helm-projectile-on)
+  )
+  
+
+
+;-----------------------
+; Hooks
+;-----------------------
+; Special buffers (whose name is surrounded by * *)
+(defun niboshi-after-change-major-mode-hook()
+  (when (string-match "^\*.*\*$" (buffer-name))
+    (setq truncate-lines t)
+    (linum-mode 0)
+    ))
+(add-hook 'after-change-major-mode-hook 'niboshi-after-change-major-mode-hook)
 
 ;-----------------------
 
