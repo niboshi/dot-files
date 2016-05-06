c4f89cfcecf2ea297670adfce1d3747167c31d79
niboshi <niboshi000@gmail.com>
2016-05-06 12:37:08 +0900

    .emacs.d: markdown

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,8 +25,9 @@
     (package-install 'helm-swoop)
     (package-install 'dtrt-indent) ; auto-detect indentation
     (package-install 'magit)
+    (package-install 'markdown-mode)
     (message "niboshi-setup: Finished")
-   ))
+    ))
 
 ;;-----------------------
 ;; Server
@@ -549,6 +550,31 @@
 ;; Disable backends except SVN
 ;; because vc often causes slowdown.
 (setq vc-handled-backends '(SVN))
+
+;;-----------------------
+;; Markdown
+;;-----------------------
+(add-hook 'markdown-mode-hook
+          (lambda()
+            ;; Override markdown preview filename
+            (defun markdown-export-file-name (&optional extension)
+              (when (buffer-file-name)
+                (unless extension
+                  (setq extension ".html"))
+                (concat
+                 (file-name-as-directory temporary-file-directory)
+                 (file-name-as-directory "emacs-markdown")
+                 (file-name-nondirectory (buffer-file-name))
+                 extension)))
+
+            ;; Select available markdown renderer
+            (let ((command-succeeded (lambda(cmd) (eq 0 (call-process shell-file-name nil nil nil shell-command-switch cmd)))))
+              (setq markdown-command
+                    (cond
+                     ((funcall command-succeeded "markdown --help") "markdown")
+                     ((funcall command-succeeded "python -c \"import markdown2\"") "python -m markdown2 --extras fenced-code-blocks")
+                     ((funcall command-succeeded "python -c \"import markdown\"") "python -m markdown -x markdown.extensions.fenced_code")
+                     (t nil))))))
 
 ;;-----------------------
 
