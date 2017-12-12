ed4a488704a3b71a65dff8df57769b660a6e86f5
niboshi <niboshi000@gmail.com>
2017-12-13 01:49:58 +0900

    .emacs.el: Remove magit-gh-pulls

--- .emacs.d/init/000-magit.el
+++ .emacs.d/init/000-magit.el
@@ -25,13 +25,3 @@
     (set-face-attribute 'smerge-refined-added         nil :foreground nil       :background nil)
     (set-face-attribute 'smerge-refined-removed       nil :foreground nil       :background nil)
     ))
-
-;;-----------------------
-;; magit-gh-pulls
-;;-----------------------
-(use-package magit-gh-pulls
-  :disable
-  :ensure t
-  :commands magit-gh-pulls-mode
-  :init
-  (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls))
